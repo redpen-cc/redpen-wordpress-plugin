8111e3482d6acd59db32d4f543561222d2ad202f
Anton Keks <anton@codeborne.com>
2016-01-12 10:49:05 +0200

    implemented test for adding script to plugin's output

--- tests/mocks.php
+++ tests/mocks.php
@@ -1,10 +1,15 @@
 <?php
 $registered_actions = [];
 
-// This function is normally provided by Wordpress
+// These functions are normally provided by Wordpress
+
 function add_action($hook, $func) {
     global $registered_actions;
     $registered_actions[$hook] = $func;
     echo "Registered hook $hook to function $func\n";
+}
+
+function plugins_url($resource_path) {
+    return 'http://localhost/wp-content/plugins/redpen/'.$resource_path;
 }
 ?>
--- tests/redpen_test.php
+++ tests/redpen_test.php
@@ -16,6 +16,12 @@ class RedPenTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('redpen_head', $registered_actions['admin_head']);
     }
 
+    public function testJsPathIsCorrectlyResolved() {
+        $expected_path = str_replace('/', '\\/', 'http://localhost/wp-content/plugins/redpen/js/plugin.js');
+        $this->expectOutputRegex('/<script src="'.$expected_path.'"><\\/script>/');
+        redpen_head();
+    }
+
     public function testRedButton() {
         $this->expectOutputRegex('/<button class="redpen button"/');
         add_redpen_button();
