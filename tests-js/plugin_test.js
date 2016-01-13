bebecca1e39757899ad8c706cd675c371a14e297
Anton Keks <anton@codeborne.com>
2016-01-13 17:27:08 +0200

    add test for selection within visual editor

--- tests-js/plugin_test.js
+++ tests-js/plugin_test.js
@@ -35,83 +35,6 @@ describe('RedpenPlugin', function() {
 
   });
 
-  describe('validation', function() {
-    var container;
-
-    var mockedValidateResponse = {
-      errors: [{
-        errors: [
-          {validator: 'Spelling', message:'Hello is spelled incorrectly', sentence:'Hello World'},
-          {validator: 'WrongSymbol', message:'You cannot use !', sentence:'Hello World!'}
-        ]
-      }]
-    };
-
-    function mockValidateJSON(validationResult, expectedDocument) {
-      redpen.validateJSON = function (args, callback) {
-        expect(args.config).toBe(mockedRedPensResponse.redpens.default);
-        expect(args.format).toBe('json2');
-        if (expectedDocument) expect(args.document).toBe(expectedDocument);
-        callback(validationResult);
-      };
-    }
-
-    beforeEach(function() {
-      container = $('<ol class="redpen-error-list"></ol>').appendTo('body');
-    });
-
-    it('WordPress has global jQuery, but not $, so define it locally', function() {
-      var textarea = mockTextArea('Hello World!');
-      try {
-        delete window.$;
-        mockValidateJSON({errors: []});
-        redpenPlugin.validate(textarea);
-      }
-      finally {
-        window.$ = jQuery;
-      }
-    });
-
-    it('displays nothing if no errors', function() {
-      mockValidateJSON({errors: []});
-      redpenPlugin.validate(mockTextArea('Hello World!', 'Hello World!'));
-      expect(container.find('li').length).toBe(0);
-    });
-
-    it('displays all errors', function() {
-      mockValidateJSON(mockedValidateResponse);
-
-      redpenPlugin.validate(mockTextArea('Hello World!'));
-
-      var items = container.find('li');
-      expect(items.length).toBe(2);
-      expect(items.hasClass('redpen-error-message')).toBe(true);
-
-      expect(items.eq(0).text()).toMatch(/Hello is spelled incorrectly/);
-      expect(items.eq(0).text()).toMatch(/Spelling/);
-
-      expect(items.eq(1).text()).toMatch(/You cannot use !/);
-      expect(items.eq(1).text()).toMatch(/WrongSymbol/);
-    });
-
-    it('selects erroneous text when clicking on error message', function() {
-      mockedValidateResponse.errors[0].errors[0].position = {
-        start: {offset: 3, line: 2}, end: {offset: 5, line: 2}
-      };
-      mockValidateJSON(mockedValidateResponse);
-
-      var textarea = mockTextArea('Hello\nWorld!');
-      spyOn(textarea[0], 'setSelectionRange');
-
-      redpenPlugin.validate(textarea);
-
-      container.find('li').eq(0).click();
-
-      expect(textarea[0].setSelectionRange).toHaveBeenCalledWith(9, 11);
-    });
-
-  });
-
   describe('getDocumentText', function() {
     it('for plain text', function() {
       var textarea = mockTextArea('Hello World!');
@@ -130,5 +53,119 @@ describe('RedpenPlugin', function() {
       var textarea = mockTextArea().hide();
       expect(redpenPlugin._getDocumentText(textarea)).toBe('Hello \nWordPress\nand the World!')
     });
+  });
+
+  describe('validation', function() {
+    var container;
+
+    var mockedValidateResponse = {
+      errors: [{
+        errors: [
+          {validator: 'Spelling', message: 'Hello is spelled incorrectly', sentence: 'Hello World'},
+          {validator: 'WrongSymbol', message: 'You cannot use !', sentence: 'Hello World!'}
+        ]
+      }]
+    };
+
+    function mockValidateJSON(validationResult, expectedDocument) {
+      redpen.validateJSON = function (args, callback) {
+        expect(args.config).toBe(mockedRedPensResponse.redpens.default);
+        expect(args.format).toBe('json2');
+        if (expectedDocument) expect(args.document).toBe(expectedDocument);
+        callback(validationResult);
+      };
+    }
+
+    beforeEach(function () {
+      container = $('<ol class="redpen-error-list"></ol>').appendTo('body');
+    });
+
+    it('WordPress has global jQuery, but not $, so define it locally', function () {
+      var textarea = mockTextArea('Hello World!');
+      try {
+        delete window.$;
+        mockValidateJSON({errors: []});
+        redpenPlugin.validate(textarea);
+      }
+      finally {
+        window.$ = jQuery;
+      }
+    });
+
+    it('displays nothing if no errors', function () {
+      mockValidateJSON({errors: []});
+      redpenPlugin.validate(mockTextArea('Hello World!', 'Hello World!'));
+      expect(container.find('li').length).toBe(0);
+    });
+
+    it('displays all errors', function () {
+      mockValidateJSON(mockedValidateResponse);
+
+      redpenPlugin.validate(mockTextArea('Hello World!'));
+
+      var items = container.find('li');
+      expect(items.length).toBe(2);
+      expect(items.hasClass('redpen-error-message')).toBe(true);
+
+      expect(items.eq(0).text()).toMatch(/Hello is spelled incorrectly/);
+      expect(items.eq(0).text()).toMatch(/Spelling/);
+
+      expect(items.eq(1).text()).toMatch(/You cannot use !/);
+      expect(items.eq(1).text()).toMatch(/WrongSymbol/);
+    });
+
+    it('highlights plain text when clicking on error message', function() {
+      mockedValidateResponse.errors[0].errors[0].position = {
+        start: {offset: 3, line: 2}, end: {offset: 5, line: 2}
+      };
+      mockValidateJSON(mockedValidateResponse);
+
+      var textarea = mockTextArea('Hello\nWorld!');
+      spyOn(textarea[0], 'setSelectionRange');
+
+      redpenPlugin.validate(textarea);
+
+      container.find('.redpen-error-message').eq(0).click();
+
+      expect(textarea[0].setSelectionRange).toHaveBeenCalledWith(9, 11);
+    });
+
+    it('highlights text in visual editor when clicking on error message', function() {
+      mockedValidateResponse.errors[0].errors[0].position = {
+        start: {offset: 1, line: 2}, end: {offset: 4, line: 2}
+      };
+      mockValidateJSON(mockedValidateResponse);
+
+      var textarea = mockTextArea().hide();
+      redpenPlugin.validate(textarea);
+
+      var editorContent = '<div><p>Hello <strong>WordPress</strong></p><p>and the World!</p></div>';
+      var selection = window.getSelection();
+      var range = document.createRange();
+      tinyMCE = {
+        activeEditor: {
+          getBody: function() {return $(editorContent)[0]},
+          selection: {
+            getSel: function() {return selection},
+            getRng: function() {return range}
+          }
+        }
+      };
+
+      spyOn(selection, 'removeAllRanges');
+      spyOn(selection, 'addRange');
+
+      spyOn(range, 'setStart');
+      spyOn(range, 'setEnd');
+
+      container.find('.redpen-error-message').eq(0).click();
+
+      expect(range.setStart).toHaveBeenCalledWith(jasmine.objectContaining({textContent: 'WordPress'}), 1);
+      expect(range.setEnd).toHaveBeenCalledWith(jasmine.objectContaining({textContent: 'WordPress'}), 4);
+
+      expect(selection.removeAllRanges).toHaveBeenCalled();
+      expect(selection.addRange).toHaveBeenCalledWith(range);
+    });
+
   });
 });
