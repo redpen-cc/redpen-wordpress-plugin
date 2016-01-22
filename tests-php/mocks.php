a93969bb385372d93b2b81c3a63e4229bdc67040
Anton Keks <anton@codeborne.com>
2016-01-22 11:55:36 +0200

    [#112211491] test that two metaboxes are added to post and page editors

--- tests-php/mocks.php
+++ tests-php/mocks.php
@@ -1,5 +1,6 @@
 <?php
 $registered_hooks = [];
+$registered_meta_boxes = [];
 
 // These functions are normally provided by Wordpress
 
@@ -13,6 +14,11 @@ function add_filter($hook, $func) {
     global $registered_hooks;
     $registered_hooks[$hook] = $func;
     echo "Registered filter hook $hook to function $func\n";
+}
+
+function add_meta_box($id, $title, $callback, $screen, $context, $priority) {
+    global $registered_meta_boxes;
+    $registered_meta_boxes[$id] = func_get_args();
 }
 
 function plugins_url($resource_path) {
