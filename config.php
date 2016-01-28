fb12424beaefa7dc003e82601f72d7aa00f67ca4
Anton Keks <anton@codeborne.com>
2016-01-28 12:17:18 +0200

    [#111428266] add description clarifying why localhost is ok here

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
 global $redpen_base_url;
+
+# The URL of RedPen Server, must be accessible from the same machine, where WordPress is running, not the browser.
+# http://localhost:8080/ is a reasonable default if you haven't changed RedPen Server's configuration.
+# It will be accessed using proxy.php, which is included with the plugin.
+
 $redpen_base_url = 'http://localhost:8080/';
 ?>
