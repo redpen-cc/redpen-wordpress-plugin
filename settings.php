9dc6b041fe19034f4deb019433fe48a7a551361c
Anton Keks <anton@codeborne.com>
2016-02-08 15:18:39 +0200

    [#113253959] add settings to Writing page, don't create a new page for only 1 configuration option

--- settings.php
+++ settings.php
@@ -1,37 +1,17 @@
 <?php
-// create custom plugin settings menu
-add_action('admin_menu', 'redpen_create_menu');
-global $redpen_base_url;
+add_action('admin_init', 'redpen_register_settings');
 
-function redpen_create_menu() {
-    add_options_page('RedPen Plugin Settings', 'RedPen', 'administrator', __FILE__, 'redpen_settings_page');
-    add_action('admin_init', 'register_redpen_settings');
+function redpen_register_settings() {
+    add_settings_section('redpen', 'RedPen Server', 'redpen_settings_section', 'writing');
+    add_settings_field('redpen_base_url', 'URL', 'redpen_setting_field', 'writing', 'redpen');
+    register_setting('writing', 'redpen_base_url');
 }
 
-
-function register_redpen_settings() {
-    //register our settings
-    register_setting('redpen-settings-group', 'redpen_base_url');
+function redpen_settings_section() {
+    echo '<p>Change the URL below if you want to use your own instance of RedPen Server.</p>';
 }
 
-function redpen_settings_page() {
-    ?>
-    <div class="wrap">
-        <h2>RedPen Server</h2>
-        <p>Change the URL below if you want to use your own instance of RedPen Server.</p>
-
-        <form method="post" action="settings.php">
-            <?php settings_fields('redpen-settings-group')?>
-            <?php do_settings_sections('redpen-settings-group')?>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">URL</th>
-                    <td><input type="text" name="redpen_base_url" value="<?=esc_attr(get_option('redpen_base_url'))?>"></td>
-                </tr>
-            </table>
-
-            <?php submit_button(); ?>
-
-        </form>
-    </div>
-<?php } ?>
+function redpen_setting_field() {
+    echo '<input type="text" name="redpen_base_url" value="' . esc_attr(get_option('redpen_base_url')). '">';
+}
+?>
