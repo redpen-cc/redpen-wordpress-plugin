2a6824d7834959ef181438c3cd7c3a7c2851c53d
Anton Keks, Dmitri Ess <anton@codeborne.com, dmitri.ess@codeborne.com>
2016-02-18 11:01:47 +0200

    new plugin version

--- redpen.php
+++ redpen.php
@@ -4,7 +4,7 @@ Plugin Name: RedPen Text Validator
 Plugin URI: http://redpen.cc
 Description: Validates posts with RedPen while editing.
 Author: Anton Keks & Takahiko Ito
-Version: 1.1
+Version: 1.1.1
 */
 $redpen_plugin_ver = '1.1';
 $redpen_plugin_root = plugin_dir_url(__FILE__);
